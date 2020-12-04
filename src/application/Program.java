c3417d0ddaee0c46553760ee7739510f3e6611ca
DanielOliveira <daniel.compositor1307@gmail.com>
2020-12-04 09:19:49 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -29,6 +29,13 @@ public class Program {
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("=== Test 3: seller findAll ===");
+		List<Seller> listAll = sellerDao.findAll();
+		
+		for (Seller obj : listAll) {
+			System.out.println(obj);
+		}
 	}
 
 }
